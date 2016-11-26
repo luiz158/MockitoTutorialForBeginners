5eae5ddb7a17a68214d436469149f47245aa84ac
Prajesh Ananthan <prajesh.ananthan@outlook.com>
2016-11-26 17:57:57 +0800

    Added new assertThat statements.

--- src/test/java/com/in28minutes/mockito/HamcrestMatcherTest.java
+++ src/test/java/com/in28minutes/mockito/HamcrestMatcherTest.java
@@ -1,40 +1,41 @@
 package com.in28minutes.mockito;
 
-import static org.hamcrest.CoreMatchers.hasItems;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.arrayContainingInAnyOrder;
-import static org.hamcrest.Matchers.arrayWithSize;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.isEmptyOrNullString;
-import static org.hamcrest.Matchers.isEmptyString;
-import static org.hamcrest.Matchers.lessThan;
-import static org.hamcrest.core.Every.everyItem;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Test;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.Every.everyItem;
 
 public class HamcrestMatcherTest {
 
-	@Test
-	public void basicHamcrestMatchers() {
-		List<Integer> scores = Arrays.asList(99, 100, 101, 105);
-		assertThat(scores, hasSize(4));
-		assertThat(scores, hasItems(100, 101));
-		assertThat(scores, everyItem(greaterThan(90)));
-		assertThat(scores, everyItem(lessThan(200)));
+    @Test
+    public void basicHamcrestMatchers() {
 
-		// String
-		assertThat("", isEmptyString());
-		assertThat(null, isEmptyOrNullString());
+        List<Integer> scores = Arrays.asList(99, 100, 101, 105);
 
-		// Array
-		Integer[] marks = { 1, 2, 3 };
+        assertThat(scores, hasSize(4));
+        assertThat(scores, hasItems(100, 101));
 
-		assertThat(marks, arrayWithSize(3));
-		assertThat(marks, arrayContainingInAnyOrder(2, 3, 1));
+        assertThat(scores, everyItem(greaterThan(90)));
+        assertThat(scores, everyItem(lessThan(200)));
 
-	}
+        assertThat(scores.get(0), is(greaterThan(80)));
+        assertThat(scores.get(0), is(lessThan(100)));
+
+        // String
+        assertThat("", isEmptyString());
+        assertThat(null, isEmptyOrNullString());
+
+        // Array
+        Integer[] marks = {1, 2, 3};
+
+        assertThat(marks, arrayWithSize(3));
+        assertThat(marks, arrayContainingInAnyOrder(2, 3, 1));
+
+    }
 }
